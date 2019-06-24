76179390c9e6c139ddccecacbb3b09cd0f51530e
Soporte Tecnico UTI <os99st@foncodes.pe>
2019-06-24 16:57:18 -0500

    Commit EDteam

---
+++ js-01/scripts.js
@@ -0,0 +1,28 @@
+/* let numero=10
+console.log(numero)
+console.log(numero + 1)
+console.log(numero * 2)
+console.log(numero / 3) */
+
+/*if (5>numero) {
+    console.log(numero)
+} */
+
+const number=10
+
+
+
+let numero=10,
+nombre="Elvis", 
+empresa="sac",
+edad, cargo, pais
+
+numero=20
+
+console.log(numero)
+
+
+
+
+    
+
---
+++ js-01/scripts1.js
@@ -0,0 +1,12 @@
+let number=4
+let userAge=7
+let galleryMainContainer=10
+let nombre= `EDteam tiene ${number} años`
+let div=`
+    <div class="container">
+        ${nombre}
+    </div>
+    `
+//console.log(nombre)
+
+document.body.innerHTML=div
